d51dd22454288f4127e9fd21f466b656aea55227
Leandro Dominguez <leandrodmz@hotmail.com>
2022-01-13 08:39:24 -0300

    Styling the menu

---
+++ blog/src/components/Header/index.js
@@ -0,0 +1,32 @@
+
+import logosvg from '../../svg/logo.svg';
+
+function Header(){
+
+    const descLogo = "Essa é a logo do Blog"
+
+    return(
+        
+        <>
+            <header className='flex-space-between'>
+                <div className="logo">
+                    <img src={logosvg} alt="descLogo"></img>
+                </div>
+
+                <div className="search">
+                    <input type="text" className="input-search" name="search"></input>
+                </div>
+
+                <ul className="menu">
+                    <li><a href="#" className="nav-link">Categories</a></li>
+                    <li><a href="#" className="nav-link">About</a></li>
+                    <li><a href="#" className="nav-link">Contact</a></li>                    
+                </ul>
+
+            </header>
+        
+        </>
+    );
+}
+
+export default Header
